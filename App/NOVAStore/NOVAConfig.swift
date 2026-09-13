e9580a1bdaf5c0b831bb2d93635d089c9b701c24
GFIOPZ <hassanyplus@gmail.com>
2026-09-13 15:04:36 +0300

    Update NOVAConfig.swift

--- App/NOVAStore/NOVAConfig.swift
+++ App/NOVAStore/NOVAConfig.swift
@@ -5,18 +5,4 @@ enum NOVAConfig {
     static let apiBaseURL =
         "https://nova-ipa.hassanyipa.workers.dev"
 
-    static var isValid: Bool {
-
-        guard
-            let url = URL(
-                string: apiBaseURL
-            ),
-            url.scheme == "https",
-            url.host != nil
-        else {
-            return false
-        }
-
-        return true
-    }
 }
